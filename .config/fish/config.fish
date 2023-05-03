3fb659bab6c761898aedc0ef9e7b87f57a314e4a
Cem Özden <ozden.cem@outlook.com>
2023-05-03 23:10:55 +0200

    Preparing fish scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,23 @@
+source ~/.config/fish/git_utility.fish
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set fish_greeting # Remove fish greeting
+    
+    # Extending PATH variable
+    fish_add_path . # Always add the current folder into the path
+    fish_add_path ~/bin
+    
+    #Add Abbreviations
+    abbr gs git status
+    abbr gco git commit
+    abbr gaa git add .
+    abbr el exa -lah
+    abbr gb git allBranches
+    abbr gp git push
+    
+    # Add aliases for common ops.
+    alias dotfiles='cd ~/.dotfiles'
+    alias dev-rust='cd ~/dev/project/rust'
+    alias dev-java='cd ~/dev/project/java'
+    alias dev-elixir='cd ~/dev/project/elixir'
 end
