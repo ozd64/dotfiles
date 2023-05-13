95c17a186756f0cbba7c1eeac615a1de02db7b26
Cem Özden <ozden.cem@outlook.com>
2023-05-13 19:59:52 +0200

    Adding Ctrl+Space and Enter combinations into LSP. Remap default tmux prefix

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -20,15 +20,26 @@ lsp.setup_servers({
     'cmake'
 })
 
-lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-	vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-	vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
-	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+    vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
+    local cmp = require('cmp')
+    local cmp_action = require('lsp-zero').cmp_action()
+
+    cmp.setup({
+        mapping = {
+            ['<CR>'] = cmp.mapping.confirm({select = false}),
+            ['<C-Space>'] = cmp.mapping.complete()
+        }
+    })
 end)
 
 lsp.setup()
