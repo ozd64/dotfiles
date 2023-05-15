6fed2c9aac1548f896835c6cd08b19102fd14496
Cem Özden <ozden.cem@outlook.com>
2023-05-16 00:12:39 +0200

    Adding code action LSP

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -30,6 +30,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
     vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "ga", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 
     local cmp = require('cmp')
