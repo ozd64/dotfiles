835e2f6e9507df527642cd6c9fe87c1dcc9151fd
Cem Özden <ozden.cem@outlook.com>
2023-06-19 21:47:03 +0200

    Removing autocomplete

--- .config/nvim/lua/core/remaps.lua
+++ .config/nvim/lua/core/remaps.lua
@@ -1,9 +1,3 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
-vim.keymap.set("i", "(", "()<left>")
-vim.keymap.set("i", "[", "[]<left>")
-vim.keymap.set("i", "{", "{}<left>")
-vim.keymap.set("i", "'", "''<left>")
-vim.keymap.set("i", "\"", "\"\"<left>")
-vim.keymap.set("i", "`", "``<left>")
